694e1208916a0a44aa7f7d6dce1bdc7993787e29
Rude <rude.ayelo@xing.com>
2019-10-15 19:06:28 +0200

    add a comment with context about the credentials fix

--- src/firebase-admin.ts
+++ src/firebase-admin.ts
@@ -6,6 +6,8 @@ try {
   firebase.initializeApp({
     credential: firebase.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
+      // Fix based on this StackOverflow answer:
+      // https://stackoverflow.com/questions/39492587/escaping-issue-with-firebase-privatekey-as-a-heroku-config-variable/41044630#41044630
       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL
     }),
